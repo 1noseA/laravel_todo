e1d894e7c716da35019dcfbbb69f1f4df90e36e0
1noseA <1noseA2L@gmail.com>
2020-07-06 10:47:49 +0900

    [Add]FoldersTableSeeder

---
+++ database/seeds/FoldersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FoldersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $title = ['プライベート', '仕事', '旅行'];
+
+        foreach ($title as $title) {
+            DB::table('folders')->insert([
+                'title' => $title,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+    }
+}
