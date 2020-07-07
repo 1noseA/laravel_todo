dc4343e03b1a684a3e794b3eca260e346aef50ce
1noseA <1noseA2L@gmail.com>
2020-07-07 14:07:42 +0900

    [Update]formatted_due_date

--- app/Task.php
+++ app/Task.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Task extends Model
 {
@@ -39,5 +40,11 @@ class Task extends Model
         }
 
         return self::STATUS[$status]['class'];
+    }
+
+    // 整形した期限日
+    public function getFormattedDueDateAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d', $this->attributes['due_date'])->format('Y/m/d');
     }
 }
